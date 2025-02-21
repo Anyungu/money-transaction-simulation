843eb12af1dcd7852d6ca8e57162a2e1885c6c8d
Anyungu Wanyungu <anyungucw@gmail.com>
2025-02-21 18:19:21 +0300

    remove js files

--- src/api/v1/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-var express_1 = require("express");
-var auth_controller_ts_1 = require("../../../modules/auth/auth.controller.ts");
-exports.authRouter = express_1.default.Router();
-exports.authRouter.route("/sign-up").post(auth_controller_ts_1.userSignUpController);
-exports.authRouter.route("/login").post(auth_controller_ts_1.userLoginController);
--- src/lib/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pino_1 = require("pino");
-var logger = (0, pino_1.default)({
-    level: "info",
-});
-exports.default = logger;
